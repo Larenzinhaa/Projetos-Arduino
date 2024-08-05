0ecf795141aa21bbeca636ba8582ab51b102f038
Ana Lauren Dourado <analaurendourado@gmail.com>
2024-08-05 15:12:07 -0300

    alteração

--- piscar_led/piscar_led.ino
+++ piscar_led/piscar_led.ino
@@ -1,6 +1,8 @@
+// programa que envia sinal auto para saída 7, aguarda 1 segundo, e envia sinal baixo para saída 7
 void setup() {
   // put your setup code here, to run once:
   pinMode (7,OUTPUT);
+  // porta configurada como saída
 
 }
 
